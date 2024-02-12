4e090bf2377a5aa8e6097a9ffeea7124f0b0d385
Mantas <62251258+DEC0YS@users.noreply.github.com>
2024-02-12 19:16:24 +0200

    MALDAUJU NELIESTI ŠITO
    Pridėjau meniu, nepatiko, bet visai gražu, daug visko reikėjo pakeisti ir perdėlioti tvarką, bet turiu veikiantį produktą. Įrašius vardus, pavardes ir pažymius ranka ir tada jiems priskyrus atsitiktinisu pažymius, perrašysite senuosius, tačiau generuojant visiškai skirtingus vardus ir pažymius, senieji liks. LABAI PRAŠAU NELIESTI KODO, NES JIS VOS VEIKIA. AČIŪ <3

--- masyvai.cpp
+++ masyvai.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 typedef struct {
     char vardas[50];
@@ -10,7 +11,7 @@ typedef struct {
     int egzaminas;
 } Studentas;
 
-// Funkcija, skirta int masyvo rūšiavimui didėjimo tvarka
+//Rusiuoju array didejimo tvarka
 void sort(int *array, int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
@@ -23,9 +24,8 @@ void sort(int *array, int size) {
     }
 }
 
-// Funkcija, skirta medianos apskaičiavimui
 double calculateMedian(int *array, int size) {
-    sort(array, size); // Rūšiuojame masyvą
+    sort(array, size);
     if (size % 2 == 0) {
         return (array[size / 2 - 1] + array[size / 2]) / 2.0;
     } else {
@@ -34,73 +34,121 @@ double calculateMedian(int *array, int size) {
 }
 
 void generateRandomGrades(Studentas *studentas) {
-    studentas->nd_kiekis = rand() % 10 + 1; // nd KIEKIS - tarp 1 ir 10
+    studentas->nd_kiekis = rand() % 10 + 1; //nd KIEKIS nuo 1 iki 10
     studentas->nd_rezultatai = (int *)malloc(studentas->nd_kiekis * sizeof(int));
     for (int i = 0; i < studentas->nd_kiekis; i++) {
-        studentas->nd_rezultatai[i] = rand() % 10 + 1; //nd - tarp 1 ir 10
+        studentas->nd_rezultatai[i] = rand() % 10 + 1; //nd balai nuo 1 iki 10
     }
-    studentas->egzaminas = rand() % 10 + 1; //egzas - tarp 1 ir 10
+    studentas->egzaminas = rand() % 10 + 1; //egzo balas nuo 1 iki 10
+}
+
+void generateRandomNamesAndGrades(Studentas *studentas) {
+    const char *vardai[] = {"Jonas", "Petras", "Antanas", "Juozas", "Kazimieras"};
+    const char *pavardes[] = {"Jonaitis", "Petraitis", "Antanaitis", "Juozaitis", "Kazimieraitis"};
+    int vardasIndex = rand() % 5;
+    int pavardeIndex = rand() % 5;
+    strcpy(studentas->vardas, vardai[vardasIndex]);
+    strcpy(studentas->pavarde, pavardes[pavardeIndex]);
+    generateRandomGrades(studentas);
 }
 
 int main() {
-    int m = 0, i, j;
+    srand(time(NULL));
+    int pasirinkimas, m = 0;
+    char metodas;
     Studentas *studentai = NULL;
-    char testi = 't', metodas, generavimas;
-
-    srand(time(NULL)); 
 
     printf("Pasirinkite skaičiavimo metodą (v - vidurkis, m - mediana): ");
     scanf(" %c", &metodas);
-    printf("Ar norite generuoti balus atsitiktinai? (t - Taip / n - Ne): ");
-    scanf(" %c", &generavimas);
 
-    while (testi == 't') {
-        studentai = (Studentas *)realloc(studentai, (m + 1) * sizeof(Studentas));
-        printf("Įveskite studento vardą ir pavardę: ");
-        scanf("%s %s", studentai[m].vardas, studentai[m].pavarde);
+    while (1) {
+        printf("Meniu:\n");
+        printf("1 - Įvesti studentų vardus rankiniu būdu\n");
+        printf("2 - Generuoti atsitiktinius pažymius\n");
+        printf("3 - Generuoti atsitiktinius studentų vardus ir jų pažymius\n");
+        printf("4 - Baigti darbą\n");
+        printf("Įveskite savo pasirinkimą: ");
+        scanf("%d", &pasirinkimas);
 
-        if (generavimas == 't') {
-            generateRandomGrades(&studentai[m]);
-        } else {
-            studentai[m].nd_rezultatai = NULL;
-            studentai[m].nd_kiekis = 0;
-            printf("Įveskite ND rezultatus (0 baigia įvedimą): ");
-            int rez = 1;
-            while (rez != 0) {
-                scanf("%d", &rez);
-                if (rez != 0) {
-                    studentai[m].nd_rezultatai = (int *)realloc(studentai[m].nd_rezultatai, (studentai[m].nd_kiekis + 1) * sizeof(int));
-                    studentai[m].nd_rezultatai[studentai[m].nd_kiekis] = rez;
-                    studentai[m].nd_kiekis++;
-                }
-            }
-            printf("Įveskite egzamino rezultatą: ");
-            scanf("%d", &studentai[m].egzaminas);
+        if (pasirinkimas == 4) {
+            break;
         }
 
-        m++;
-        printf("Ar norite įvesti dar vieną studentą? (t/n): ");
-        scanf(" %c", &testi);
-    }
+        switch (pasirinkimas) {
+            case 1: {
+                char testi = 't';
+                do {
+                    studentai = (Studentas *)realloc(studentai, (m + 1) * sizeof(Studentas));
+                    printf("Įveskite studento vardą ir pavardę: ");
+                    scanf("%s %s", studentai[m].vardas, studentai[m].pavarde);
 
-    for (i = 0; i < m; i++) {
+                    studentai[m].nd_rezultatai = NULL;
+                    studentai[m].nd_kiekis = 0;
+                    printf("Įveskite ND rezultatus (0 baigia įvedimą): ");
+                    int rez;
+                    do {
+                        scanf("%d", &rez);
+                        if (rez != 0) {
+                            studentai[m].nd_rezultatai = (int *)realloc(studentai[m].nd_rezultatai, (studentai[m].nd_kiekis + 1) * sizeof(int));
+                            studentai[m].nd_rezultatai[studentai[m].nd_kiekis++] = rez;
+                        }
+                    } while (rez != 0);
+
+                    printf("Įveskite egzamino rezultatą: ");
+                    scanf("%d", &studentai[m].egzaminas);
+
+                    m++;
+                    printf("Ar norite įvesti dar vieną studentą? (t/n): ");
+                    scanf(" %c", &testi);
+                } while (testi == 't');
+                break;
+            }
+            case 2: {
+                for (int i = 0; i < m; i++) {
+                    generateRandomGrades(&studentai[i]);
+                }
+                printf("Visiems esamiems studentams sėkmingai sugeneruoti atsitiktiniai pažymiai.\n");
+                break;
+            }
+            case 3: {
+                char testi = 't';
+                    do {
+                        studentai = (Studentas *)realloc(studentai, (m + 1) * sizeof(Studentas));
+                        generateRandomNamesAndGrades(&studentai[m]);
+                        m++;
+                        printf("Sugeneruotas naujas studentas. Ar norite generuoti dar vieną? (t/n): ");
+                        scanf(" %c", &testi);
+                } while (testi == 't');
+                break;
+                }
+            case 4: {
+                printf("Programa išjungta.\n");
+                break;
+            }
+            default: {
+                printf("Neteisingai įvesti pasirinkimo duomenys. Bandykite dar kartą.\n");
+                break;
+            }
+        }
+    } for (int i = 0; i < m; i++) {
         double galutinis;
         if (metodas == 'v' || metodas == 'V') {
             double vidurkis = 0;
-            for (j = 0; j < studentai[i].nd_kiekis; j++) {
+            for (int j = 0; j < studentai[i].nd_kiekis; j++) {
                 vidurkis += studentai[i].nd_rezultatai[j];
             }
-            vidurkis /= studentai[i].nd_kiekis;
+            if (studentai[i].nd_kiekis > 0) {
+                vidurkis /= studentai[i].nd_kiekis;
+            }
             galutinis = 0.4 * vidurkis + 0.6 * studentai[i].egzaminas;
-        } else if (metodas == 'm' || metodas == 'M') { 
+        } else if (metodas == 'm' || metodas == 'M') {
             double mediana = calculateMedian(studentai[i].nd_rezultatai, studentai[i].nd_kiekis);
             galutinis = 0.4 * mediana + 0.6 * studentai[i].egzaminas;
         }
         printf("%s %s: Galutinis balas = %.2f\n", studentai[i].vardas, studentai[i].pavarde, galutinis);
     }
 
-    // Atminties valymas
-    for (i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++) {
         free(studentai[i].nd_rezultatai);
     }
     free(studentai);

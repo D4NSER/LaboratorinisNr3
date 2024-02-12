8c0c8f6e41c0f5defd8aebd50156e5c938482c4e
Mantas <62251258+DEC0YS@users.noreply.github.com>
2024-02-12 16:38:45 +0200

    Update
    Pridėjau funkciją mediana, kad galima būtų pasirinkti, kaip nori gauti atsakymus ir prieš kiekvieną duomenų įvedimą pridėjau labai smagią ir sunkią (sadly) eilutę (cin.ignore(numeric_limits<streamline>max();), kad  po duomenų įvedimo viskas, kas nereikalinga būtų ištrinta ir kodas nenulūžtų dėl neteisingų charakter'ių įvedimo

--- vektoriai.cpp
+++ vektoriai.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 double vidurkis(const std::vector<int>& nd) {
     double suma = 0.0;
@@ -10,11 +11,27 @@ double vidurkis(const std::vector<int>& nd) {
     return suma / nd.size();
 }
 
+double mediana(std::vector<int> nd) {
+    size_t dydis = nd.size();
+    std::sort(nd.begin(), nd.end());
+    if (dydis % 2 == 0) {
+        return (nd[dydis / 2 - 1] + nd[dydis / 2]) / 2.0;
+    } else {
+        return nd[dydis / 2];
+    }
+}
+
 int main() {
     std::vector<std::string> vardai;
-    std::vector<double> galutiniai_balai;
+    std::vector<double> galutinis_pazymys;
     std::string vardas;
     int pazymys;
+    char skaiciavimoBudas;
+
+    std::cout << "Pasirinkte skaičiavimo būdą (v - vidurkis arba m - mediana): ";
+    std::cin >> skaiciavimoBudas;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //tiesiog ignoruos viska po paskutinio ivesto charo
+
 
     while (true) {
         std::cout << "Iveskite studento varda ir pavarde (arba 'baigti' norint uzbaigti): ";
@@ -25,19 +42,24 @@ int main() {
 
         std::vector<int> nd;
         std::cout << "Ivedinėkite namų darbų rezultatus (0 norint baigti): ";
-        while (true) {
-            std::cin >> pazymys;
-            if (pazymys == 0) break;
+        while (std::cin >> pazymys && pazymys != 0) {
             nd.push_back(pazymys);
         }
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //Valo viska, kas netinka eiluteje
 
         int egz;
         std::cout << "Iveskite egzamino rezultata: ";
         std::cin >> egz;
-        std::cin.ignore();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-        double galutinis = 0.4 * vidurkis(nd) + 0.6 * egz;
-        galutiniai_balai.push_back(galutinis);
+        double galutinis;
+        if (skaiciavimoBudas == 'v' || skaiciavimoBudas == 'V'){
+            galutinis = 0.6 * egz + 0.4 * vidurkis(nd);
+        }else {
+            galutinis = 0.6 * egz + 0.4 * mediana(nd);
+        }
+        galutinis_pazymys.push_back(galutinis);
 
         std::cout << vardas << " galutinis balas: " << galutinis << std::endl;
     }

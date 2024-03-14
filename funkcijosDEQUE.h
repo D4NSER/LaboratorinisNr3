2e33247b461c9c9f25470eacebd57c82d0099824
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-03-14 12:48:26 +0200

    Update funkcijosDEQUE.h

--- funkcijosDEQUE.h
+++ funkcijosDEQUE.h
@@ -6,8 +6,6 @@
 
 void readDataDeque(std::deque<Studentas>& studentai, const std::string& failoVardas);
 void generateStudentFilesDeque(int size);
-void sortStudentsDeque(std::deque<Studentas>& studentai);
-void writeStudentsToFile(const std::deque<Studentas>& students, const std::string& fileName);
-void divideStudentsDeque(int size);
+void divideStudentsDeque(const std::string& fileName);
 
 #endif // FUNKCIJOSDEQUE_H

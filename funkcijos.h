ebf0eb55ad8f77ba5445f02d8799d63c768d2a96
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-03-14 22:27:37 +0200

    Add files via upload

---
+++ funkcijos.h
@@ -0,0 +1,15 @@
+#ifndef FUNKCIJOS_H
+#define FUNKCIJOS_H
+
+#include "studentas.h"
+#include <vector>
+#include <string>
+
+void spausdintiGalutiniusBalus(const std::vector<Studentas>& studentai, const std::string& isvedimoFailoVardas = "", int rusiavimoTipas = 1);
+void manualInput(std::vector<Studentas>& studentai);
+void generateGradesOnly(std::vector<Studentas>& studentai);
+void readFileDataFromFile(std::vector<Studentas>& studentai, const std::string& failoVardas);
+void generateStudentFiles(const std::vector<int>& sizes);
+void rusiuotiStudentus(const std::vector<int>& sizes);
+
+#endif

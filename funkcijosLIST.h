7b439e1148a5957ed4e8292d3e3af4da4d4ff26a
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-03-18 22:16:41 +0200

    Update funkcijosLIST.h

--- funkcijosLIST.h
+++ funkcijosLIST.h
@@ -7,5 +7,7 @@
 void readDataList(std::list<Studentas>& studentai, const std::string& failoVardas);
 void generateStudentFilesList(int size);
 void divideStudentsList(const std::string& failoVardas);
+void divideStudentsList2(const std::string &failoVardas);
+void divideStudentsList3(const std::string &failoVardas);
 
 #endif // FUNKCIJOSLIST_H

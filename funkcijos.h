dec94613461a632e6baec85e451236d53d424d71
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-03-26 00:07:41 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -1,14 +1,20 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+#ifndef FUNCTIONS_OLD_H
+#define FUNCTIONS_OLD_H
 
 #include <vector>
+#include <string>
+#include "studentas.h"
 
-enum class ContainerType { None, Vector };
-enum class Action { None, Generate, Sort };
-ContainerType getContainerChoice();
-Action getActionChoice();
+void surusiuotiKategorijas(const std::vector<Studentas> &studentai, std::vector<Studentas> &vargsiukai, std::vector<Studentas> &kietiakai, double &laikas);
+void irasymasFaile(const std::vector<Studentas> &studentai, const std::string &failoPavadinimas, double &laikas);
+void rusiuotiStudentusIrIrasymas(const std::vector<Studentas> &studentai, double &laikas);
+void generuotiStudentuFailus(const std::vector<int>& sizes);
+void nuskaitymas(std::vector<Studentas>& studentai, const std::string& failoPavadinimas, double &laikas);
+bool rusiuotiPagalVarda(const Studentas& a, const Studentas& b);
+bool rusiuotiPagalPavarde(const Studentas& a, const Studentas& b);
+bool rusiuotiPagalGalutiniVidurki(const Studentas& a, const Studentas& b);
+bool rusiuotiPagalGalutiniMediana(const Studentas& a, const Studentas& b);
+void spausdinimas(const std::vector<Studentas>& studentai, const std::string& isvedimoFailas = "");
+std::string pasirinktiFaila();
 
-void performAction(ContainerType containerChoice, Action actionChoice, const std::vector<int>& sizes);
-void runApp();
-
-#endif
+#endif // FUNCTIONS_OLD_H

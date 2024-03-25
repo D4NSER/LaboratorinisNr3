82f43c2a138b01b195e2ab293aeb94deae9ee65f
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-03-26 00:07:02 +0200

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -1,73 +1,28 @@
-#include "funkcijosDEQUE.h"
-#include "funkcijosVECTOR.h"
-#include "funkcijosLIST.h"
-#include "funkcijos.h"
-#include "studentas.h"
+#include "functions_vector.h"
 #include <iostream>
 #include <vector>
 #include <limits>
-#include <deque>
-#include <list>
-#include <string>
 #include <chrono>
-#include <algorithm>
-#include <fstream>
-#include <sstream>
-#include <iomanip>
-#include <random>
 #include <filesystem>
-#include <cstdlib>
-#include <ctime>
-#include <limits>
-#include <stdexcept>
 
-enum class ContainerType { None, Vector, List, Deque };
 enum class Action { None, Generate, Sort };
-
-ContainerType getContainerChoice() {
-    int choice;
-    while (true) {
-        std::cout << "Pasirinkite konteinerio tipą:\n"
-                  << "1. Vector\n"
-                  << "2. List\n"
-                  << "3. Deque\n"
-                  << "Pasirinkimas: ";
-        std::cin >> choice;
-        if (std::cin.fail()) {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Įveskite skaičių.\n";
-        } else if (choice >= 1 && choice <= 3) {
-            break;
-        } else {
-            std::cout << "Netinkamas pasirinkimas. Prašome bandyti dar kartą.\n";
-        }
-    }
-
-    switch (choice) {
-        case 1: return ContainerType::Vector;
-        case 2: return ContainerType::List;
-        case 3: return ContainerType::Deque;
-        default: return ContainerType::None;
-    }
-}
 
 Action getActionChoice() {
     int choice;
     while (true) {
-        std::cout << "Pasirinkite veiksmą:\n"
-                  << "1. Generuoti studentų failus\n"
-                  << "2. Rūšiuoti esamus studentų failus\n"
+        std::cout << "Pasirinkite veiksma:\n"
+                  << "1. Generuoti studentu failus\n"
+                  << "2. Rusiuoti esamus studentu failus\n"
                   << "Pasirinkimas: ";
         std::cin >> choice;
         if (std::cin.fail()) {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Įveskite skaičių.\n";
+            std::cout << "Iveskite skaiciu.\n";
         } else if (choice == 1 || choice == 2) {
             break;
         } else {
-            std::cout << "Netinkamas pasirinkimas. Prašome bandyti dar kartą.\n";
+            std::cout << "Netinkamas pasirinkimas. Prasome bandyti dar karta.\n";
         }
     }
 
@@ -92,67 +47,43 @@ std::string getSortingFileChoice() {
     }
 
     if (files.empty()) {
-        std::cout << "Nerasti studentų failai.\n";
+        std::cout << "Studentu failu nerasta.\n";
         return "";
     }
 
-    std::cout << "Pasirinkite failą, kurį norite rūšiuoti: ";
+    std::cout << "Pasirinkite, kuri studentu faila norite rusiuoti: ";
     int choice;
     std::cin >> choice;
 
     if (std::cin.fail() || choice < 1 || choice > files.size()) {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Netinkamas pasirinkimas. Įveskite iš naujo.\n";
+        std::cout << "Netinkamas pasirinkimas. Bandykite dar karta.\n";
         return "";
     }
 
     return files[choice - 1];
 }
 
-void performAction(ContainerType containerChoice, Action actionChoice, const std::vector<int>& sizes) {
+void performAction(Action actionChoice, const std::vector<int>& sizes) {
     if (actionChoice == Action::Generate) {
         for (int size : sizes) {
             auto start = std::chrono::high_resolution_clock::now();
-            switch (containerChoice) {
-                case ContainerType::Vector:
-                    generateStudentFilesVector(size);
-                    break;
-                case ContainerType::List:
-                    generateStudentFilesList(size);
-                    break;
-                case ContainerType::Deque:
-                    generateStudentFilesDeque(size);
-                    break;
-                default:
-                    std::cout << "Nepasirinktas tinkamas konteinerio tipas.\n";
-                    break;
-            }
+            generateStudentFilesVector(size);
             auto end = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double> elapsed = end - start;
-            std::cout << "Užimtas laikas generuojant šio dydžio failą: " << size << ": " << elapsed.count() << "s\n";
+            std::cout << "Failu generavimas uztruko " << size << ": " << elapsed.count() << "s\n";
         }
     } else if (actionChoice == Action::Sort) {
         std::string filename = getSortingFileChoice();
         if (!filename.empty()) {
             auto start = std::chrono::high_resolution_clock::now();
-            switch (containerChoice) {
-                case ContainerType::Vector:
-                    rusiuotStudentusVector(filename);
-                    break;
-                case ContainerType::List:
-                    divideStudentsList(filename);
-                    break;
-                case ContainerType::Deque:
-                    divideStudentsDeque(filename);
-                    break;
-                default:
-                    std::cout << "Netinkamas konteinerio pasirinkimas.\n";
-                    break;
-            }
+            //rusiuotStudentusVector1(filename); 
+            //rusiuotStudentusVector2(filename); 
+            rusiuotStudentusVector3(filename); 
             auto end = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double> elapsed = end - start;
-            std::cout << "Užimtas laikas rūšiojant ir skaidant studentus: " << filename << ": " << elapsed.count() << "s\n";
+            std::cout << "Rusiavimas ir skirstymas faile " << filename << ": " << elapsed.count() << "s\n";
         }
     }
 }
@@ -163,24 +94,22 @@ void runApp() {
     bool exitProgram = false;
 
     while (!exitProgram) {
-        ContainerType containerChoice = getContainerChoice();
-        if (containerChoice == ContainerType::None) {
-            continue;
-        }
-
         Action actionChoice = getActionChoice();
         if (actionChoice == Action::None) {
             continue;
         }
 
-        performAction(containerChoice, actionChoice, sizes);
+        performAction(actionChoice, sizes);
 
-        std::cout << "Ar norite tęsti?? (Y/N): ";
+        std::cout << "Norite testi? (t/n): ";
         char userChoice;
         std::cin >> userChoice;
         if (userChoice == 'N' || userChoice == 'n') {
             exitProgram = true;
         }
+        else if (userChoice == 'T' || userChoice == 'T')
+        {
+            continue;
+        }
     }
 }
-

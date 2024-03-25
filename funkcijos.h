4c4e5bb69a4399812caab0f8e4bcb75cb4ec37ac
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-03-26 00:05:51 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -1,15 +1,14 @@
-#ifndef FUNKCIJOS_H
-#define FUNKCIJOS_H
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 
-#include "studentas.h"
 #include <vector>
-#include <string>
 
-void spausdintiGalutiniusBalus(const std::vector<Studentas>& studentai, const std::string& isvedimoFailoVardas = "", int rusiavimoTipas = 1);
-void manualInput(std::vector<Studentas>& studentai);
-void generateGradesOnly(std::vector<Studentas>& studentai);
-void readFileDataFromFile(std::vector<Studentas>& studentai, const std::string& failoVardas);
-void generateStudentFiles(const std::vector<int>& sizes);
-void rusiuotiStudentus(const std::vector<int>& sizes);
+enum class ContainerType { None, Vector };
+enum class Action { None, Generate, Sort };
+ContainerType getContainerChoice();
+Action getActionChoice();
+
+void performAction(ContainerType containerChoice, Action actionChoice, const std::vector<int>& sizes);
+void runApp();
 
 #endif

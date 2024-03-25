533e6c5d167bd11c1a3b0f4af50659081fddabf1
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-03-26 00:08:01 +0200

    Update funkcijosVECTOR.h

--- funkcijosVECTOR.h
+++ funkcijosVECTOR.h
@@ -1,13 +1,13 @@
-#ifndef FUNKCIJOSVECTOR_H
-#define FUNKCIJOSVECTOR_H
+#ifndef FUNCTIONS_VECTOR_H
+#define FUNCTIONS_VECTOR_H
 
 #include "studentas.h"
 #include <vector>
 
 void readDataVector(std::vector<Studentas>& studentai, const std::string& failoVardas);
 void generateStudentFilesVector(int size);
-void rusiuotStudentusVector(const std::string& failoVardas);
-void rusiuotStudentusVector2(const std::string& failoVardas);
-void rusiuotStudentusVector3(const std::string &failoVardas);
 
-#endif // FUNKCIJOSVECTOR_H
+void rusiuotStudentusVector1(const std::string& failoVardas);
+void rusiuotStudentusVector2(const std::string& failoVardas);
+void rusiuotStudentusVector3(const std::string& failoVardas);
+#endif 

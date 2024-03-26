2d4761b0ce241d09d8954afa79008d87fc0b901a
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-03-26 16:08:23 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -1,20 +1,15 @@
-#ifndef FUNCTIONS_OLD_H
-#define FUNCTIONS_OLD_H
+#ifndef FUNKCIJOS_H
+#define FUNKCIJOS_H
 
+#include "studentas.h"
 #include <vector>
 #include <string>
-#include "studentas.h"
 
-void surusiuotiKategorijas(const std::vector<Studentas> &studentai, std::vector<Studentas> &vargsiukai, std::vector<Studentas> &kietiakai, double &laikas);
-void irasymasFaile(const std::vector<Studentas> &studentai, const std::string &failoPavadinimas, double &laikas);
-void rusiuotiStudentusIrIrasymas(const std::vector<Studentas> &studentai, double &laikas);
-void generuotiStudentuFailus(const std::vector<int>& sizes);
-void nuskaitymas(std::vector<Studentas>& studentai, const std::string& failoPavadinimas, double &laikas);
-bool rusiuotiPagalVarda(const Studentas& a, const Studentas& b);
-bool rusiuotiPagalPavarde(const Studentas& a, const Studentas& b);
-bool rusiuotiPagalGalutiniVidurki(const Studentas& a, const Studentas& b);
-bool rusiuotiPagalGalutiniMediana(const Studentas& a, const Studentas& b);
-void spausdinimas(const std::vector<Studentas>& studentai, const std::string& isvedimoFailas = "");
-std::string pasirinktiFaila();
+void spausdintiGalutiniusBalus(const std::vector<Studentas>& studentai, const std::string& isvedimoFailoVardas = "", int rusiavimoTipas = 1);
+void manualInput(std::vector<Studentas>& studentai);
+void generateGradesOnly(std::vector<Studentas>& studentai);
+void readFileDataFromFile(std::vector<Studentas>& studentai, const std::string& failoVardas);
+void generateStudentFiles(const std::vector<int>& sizes);
+void rusiuotiStudentus(const std::vector<int>& sizes);
 
-#endif // FUNCTIONS_OLD_H
+#endif

1869ba383174c7b6907dee2d9596fcfabac94581
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-04-29 11:07:15 +0300

    Update funkcijosVECTOR.h

--- funkcijosVECTOR.h
+++ funkcijosVECTOR.h
@@ -4,11 +4,40 @@
 #include "studentas.h"
 #include <vector>
 
+/**
+ * @brief Įrašo studentų duomenis į vektorių iš failo.
+ * 
+ * @param studentai Vektorius, į kurį įrašomi studentų duomenys.
+ * @param failoVardas Failo pavadinimas, iš kurio skaitomi duomenys.
+ */
 void readDataVector(std::vector<Studentas>& studentai, const std::string& failoVardas);
+
+/**
+ * @brief Generuoja atsitiktinius studentų failus pagal nurodytą dydį.
+ * 
+ * @param size Studentų failų dydis.
+ */
 void generateStudentFilesVector(int size);
 
+/**
+ * @brief Rusiuoja studentų duomenis pagal vidurkį ir įrašo į failą.
+ * 
+ * @param failoVardas Failo pavadinimas, į kurį įrašomi surikiuoti studentų duomenys.
+ */
 void rusiuotStudentusVector(const std::string& failoVardas);
+
+/**
+ * @brief Rusiuoja studentų duomenis pagal vidurkį kitu algoritmu ir įrašo į failą.
+ * 
+ * @param failoVardas Failo pavadinimas, į kurį įrašomi surikiuoti studentų duomenys.
+ */
 void rusiuotStudentusVector2(const std::string& failoVardas);
+
+/**
+ * @brief Rusiuoja studentų duomenis pagal medianą ir įrašo į failą.
+ * 
+ * @param failoVardas Failo pavadinimas, į kurį įrašomi surikiuoti studentų duomenys.
+ */
 void rusiuotStudentusVector3(const std::string& failoVardas);
 
 #endif // FUNKCIJOSVECTOR_H

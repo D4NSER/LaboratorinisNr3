a1a02b40e5dca087c666016feb9afc0d7d27f067
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-04-29 11:07:26 +0300

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -1,15 +1,43 @@
-#ifndef FUNKCIJOS_H
-#define FUNKCIJOS_H
+#ifndef FUNKCIJOSVECTOR_H
+#define FUNKCIJOSVECTOR_H
 
 #include "studentas.h"
 #include <vector>
-#include <string>
 
-void spausdintiGalutiniusBalus(const std::vector<Studentas>& studentai, const std::string& isvedimoFailoVardas = "", int rusiavimoTipas = 1);
-void manualInput(std::vector<Studentas>& studentai);
-void generateGradesOnly(std::vector<Studentas>& studentai);
-void readFileDataFromFile(std::vector<Studentas>& studentai, const std::string& failoVardas);
-void generateStudentFiles(const std::vector<int>& sizes);
-void rusiuotiStudentus(const std::vector<int>& sizes);
+/**
+ * @brief Įrašo studentų duomenis į vektorių iš failo.
+ * 
+ * @param studentai Vektorius, į kurį įrašomi studentų duomenys.
+ * @param failoVardas Failo pavadinimas, iš kurio skaitomi duomenys.
+ */
+void readDataVector(std::vector<Studentas>& studentai, const std::string& failoVardas);
 
-#endif
+/**
+ * @brief Generuoja atsitiktinius studentų failus pagal nurodytą dydį.
+ * 
+ * @param size Studentų failų dydis.
+ */
+void generateStudentFilesVector(int size);
+
+/**
+ * @brief Rusiuoja studentų duomenis pagal vidurkį ir įrašo į failą.
+ * 
+ * @param failoVardas Failo pavadinimas, į kurį įrašomi surikiuoti studentų duomenys.
+ */
+void rusiuotStudentusVector(const std::string& failoVardas);
+
+/**
+ * @brief Rusiuoja studentų duomenis pagal vidurkį kitu algoritmu ir įrašo į failą.
+ * 
+ * @param failoVardas Failo pavadinimas, į kurį įrašomi surikiuoti studentų duomenys.
+ */
+void rusiuotStudentusVector2(const std::string& failoVardas);
+
+/**
+ * @brief Rusiuoja studentų duomenis pagal medianą ir įrašo į failą.
+ * 
+ * @param failoVardas Failo pavadinimas, į kurį įrašomi surikiuoti studentų duomenys.
+ */
+void rusiuotStudentusVector3(const std::string& failoVardas);
+
+#endif // FUNKCIJOSVECTOR_H

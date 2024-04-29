4b1f9eb78785a062cff18d09d97118bbcb324b2e
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-04-29 11:11:58 +0300

    Add files via upload

---
+++ html/search/related_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['operator_3c_3c_0',['operator&lt;&lt;',['../class_studentas.html#a35a55ed851dc9695ac44fbc880766631',1,'Studentas']]],
+  ['operator_3e_3e_1',['operator&gt;&gt;',['../class_studentas.html#a7881fc1a8559fe66fee6bf84b1f79968',1,'Studentas']]]
+];

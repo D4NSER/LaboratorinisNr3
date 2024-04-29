088de3bd2c7c5dde2a618d65cb4d9710894600eb
Mantas <62251258+D4NSER@users.noreply.github.com>
2024-04-29 11:07:38 +0300

    Update app.h

--- app.h
+++ app.h
@@ -6,12 +6,42 @@
 #include "studentas.h"
 #include "funkcijos.h"
 
+/**
+ * @brief Enumeracija ContainerType apibrėžia konteinerio tipus.
+ */
 enum class ContainerType { None, Vector };
+
+/**
+ * @brief Enumeracija Action apibrėžia veiksmus.
+ */
 enum class Action { None, Generate, Sort };
+
+/**
+ * @brief Gauti konteinerio pasirinkimą iš vartotojo.
+ * 
+ * @return Konteinerio pasirinkimas.
+ */
 ContainerType getContainerChoice();
+
+/**
+ * @brief Gauti veiksmo pasirinkimą iš vartotojo.
+ * 
+ * @return Veiksmo pasirinkimas.
+ */
 Action getActionChoice();
 
+/**
+ * @brief Atlikti veiksmą, priklausomai nuo konteinerio ir veiksmo pasirinkimo.
+ * 
+ * @param containerChoice Konteinerio pasirinkimas.
+ * @param actionChoice Veiksmo pasirinkimas.
+ * @param sizes Studentų failų dydžių vektorius.
+ */
 void performAction(ContainerType containerChoice, Action actionChoice, const std::vector<int>& sizes);
+
+/**
+ * @brief Paleisti programą.
+ */
 void runApp();
 
-#endif
+#endif // FUNCTIONS_H
